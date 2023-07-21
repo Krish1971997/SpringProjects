1c9dc7a22691e5e13deeeef3677be69bc7dd3092
Krish1971997 <krishnakumar1971997@gmail.com>
2023-07-21 23:42:30 +0530

    changes 2nd time

--- src/main/java/com/test/quizapp/QuizappApplication.java
+++ src/main/java/com/test/quizapp/QuizappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuizappApplication {
 
-	//changed from eclipse
+	//changed from 2nd time
 	public static void main(String[] args) {
 		SpringApplication.run(QuizappApplication.class, args);
 	}
